5d616168db109e74127fa4b35e1a795b3330f0ab
erik-elf <erik.lima.f@outlook.com>
2022-05-04 00:51:04 -0300

    Dia 2 turu

--- nlwReturn/src/components/WidgetForm/Steps/FeedBackTypeStep.tsx
+++ nlwReturn/src/components/WidgetForm/Steps/FeedBackTypeStep.tsx
@@ -16,6 +16,7 @@ export function FeedBackTypeStep({onClickFeedBackChanged}: FeedBackTypeProps) {
       { Object.entries(FeedbackType).map(([key, value]) => {
           return (
             <button
+            
             className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
             onClick={ () => onClickFeedBackChanged(key as feedbackTypes) }
             type="button"
