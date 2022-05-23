8aeeef65cf4cd8d07997d679e26fef69e7a7e66a
Erik.lima <56979306+Erik-EFL@users.noreply.github.com>
2022-05-22 21:43:53 -0300

    Adicionando botões e text area aos feedbacks

--- nlwReturn/tailwind.config.js
+++ nlwReturn/tailwind.config.js
@@ -6,10 +6,18 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500:  '#8257e6'
+
         }
+      },
+      borderRadios: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
